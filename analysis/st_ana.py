45f755682a6215be1c9a5633c5d336a8e3690356
Wang, Boqun <wang.iostream@gmail.com>
2015-09-30 10:14:08 +0900

    Change the format of output messages and the style of plots

--- analysis/st_ana.py
+++ analysis/st_ana.py
@@ -17,13 +17,15 @@ refpd=[]
 for line in open(args.filename).readlines():
     if line[0]=='#':
         continue
+    if line.strip()=='':
+        continue
     if n%5==0:
         mainpd.append([])
         refpd.append([])
     mainpd[-1].append(float(line.strip().split()[1]))
     refpd[-1].append(float(line.strip().split()[2]))
     n=n+1
-print len(mainpd)
+print "Total number of data points: ", len(mainpd)
 
 main_mean=[np.mean(item) for item in mainpd]
 main_std=[np.std(item) for item in mainpd]
@@ -33,35 +35,34 @@ ratio=[main_mean[i]/ref_mean[i] for i in xrange(len(main_mean))]
 ratio_err=[sqsum([main_std[i]/main_mean[i],ref_std[i]/ref_mean[i]])*ratio[i] for i in xrange(len(main_mean))]
 x=[i*1 for i in xrange(len(ratio))]
 
-f0,ax0 = plt.subplots()
-f0.set_size_inches(8,6)
-ax0.errorbar(x,map(lambda x:x*1e6,main_mean),yerr=map(lambda x:x*1e6, main_std),fmt='o')
+f,(ax0,ax1,ax2) = plt.subplots(3,sharex=True)
+f.set_size_inches(8,6)
 
-f1,ax1 = plt.subplots()
-f1.set_size_inches(8,6)
-ax1.errorbar(x,map(lambda x:x*1e6,ref_mean),yerr=map(lambda x:x*1e6, ref_std),fmt='o')
-
-f2,ax2 = plt.subplots()
-f2.set_size_inches(16,12)
+ax0.set_title("MainPD",fontsize=14,fontweight="bold")
+ax0.set_ylabel("MainPD [uA]",fontsize=14,fontweight="bold")
+ax1.set_title("RefPD",fontsize=14,fontweight="bold")
+ax1.set_ylabel("RefPD [uA]",fontsize=14,fontweight="bold")
+ax2.set_title("MainPD / RefPD",fontsize=14,fontweight="bold")
+ax2.set_ylabel("Ratio",fontsize=14,fontweight="bold")
 ax2.set_xlabel("Time [min]",fontsize=14,fontweight="bold")
-ax2.set_ylabel("MainPD/RefPD",fontsize=14,fontweight="bold")
-for tick in ax2.xaxis.get_major_ticks():
-    tick.label1.set_fontsize(14)
-for tick in ax2.yaxis.get_major_ticks():
-    tick.label1.set_fontsize(14)
+
+ax0.errorbar(x,map(lambda x:x*1e6,main_mean),yerr=map(lambda x:x*1e6, main_std),fmt='o')
+ax1.errorbar(x,map(lambda x:x*1e6,ref_mean),yerr=map(lambda x:x*1e6, ref_std),fmt='o')
 ax2.errorbar(x,ratio,ratio_err,fmt='o',linewidth=3)
 
 print "main pd:"
-print np.mean(main_mean),np.std(main_mean),np.std(main_mean)/np.mean(main_mean)
-print max(main_mean),min(main_mean),(max(main_mean)-min(main_mean))/min(main_mean)
-print "ref pd:"
-print np.mean(ref_mean),np.std(ref_mean),np.std(ref_mean)/np.mean(ref_mean)
-print max(ref_mean),min(ref_mean),(max(ref_mean)-min(ref_mean))/min(ref_mean)
-print "ratio:"
-print np.mean(ratio),np.std(ratio),np.std(ratio)/np.mean(ratio)
-print max(ratio),min(ratio),(max(ratio)-min(ratio))/min(ratio)
+print "mean: ",np.mean(main_mean),", err: ",np.std(main_mean), ", relative err: ",np.std(main_mean)/np.mean(main_mean)*100," %"
+print "max: ",max(main_mean),", min: ",min(main_mean),", (max-min)/min: ", (max(main_mean)-min(main_mean))/min(main_mean)*100, "%"
 
-f2.show()
+print "ref pd:"
+print "mean: ",np.mean(ref_mean),", err: ",np.std(ref_mean), ", relative err: ",np.std(ref_mean)/np.mean(ref_mean)*100," %"
+print "max: ",max(ref_mean),", min: ",min(ref_mean),", (max-min)/min: ", (max(ref_mean)-min(ref_mean))/min(ref_mean)*100, "%"
+
+print "ratio:"
+print "mean: ",np.mean(ratio),", err: ",np.std(ratio), ", relative err: ",np.std(ratio)/np.mean(ratio)*100," %"
+print "max: ",max(ratio),", min: ",min(ratio),", (max-min)/min: ", (max(ratio)-min(ratio))/min(ratio)*100, "%"
+
+f.show()
 
 raw_input()
 
